198d8bdec9e6019db4da47bd9714dc00e1e5732d
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2024-07-12 15:20:06 +0700

    added clerk for authentication

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,25 @@
+"use server";
+import mongoose from "mongoose";
+
+// singleton pattern
+let isConnected = false;
+export const connectToDatabase = async () => {
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL is not defined");
+  }
+
+  if (isConnected) {
+    console.log("=> Connect successful, using existing connection");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "Ucademy",
+    });
+    isConnected = true;
+    console.log("=> Using new database connection");
+  } catch (error) {
+    console.error("=> Connect failed", error);
+  }
+};
